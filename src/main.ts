c0ffc284bccffafea641566a43af7e08c92fd5ac
santiolaciregui <54482877+santiolaciregui@users.noreply.github.com>
2024-10-24 20:11:14 -0300

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { RateLimiterMiddleware } from './middleware/rate-limiter.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,16 @@ async function bootstrap() {
   
   const rateLimiter = new RateLimiterMiddleware();
 
-  app.use(rateLimiter.use.bind(rateLimiter));
+  app.use(rateLimiter.use.bind(rateLimiter))
+  
+  const config = new DocumentBuilder()
+       .setTitle('NestJS OpenAI API')
+       .setDescription('API documentation for the NestJS OpenAI integration')
+       .setVersion('1.0')
+       .addTag('OpenAI')
+       .build();
+     const document = SwaggerModule.createDocument(app, config);
+     SwaggerModule.setup('api/docs', app, document);;
 
   const logger = new Logger('Bootstrap');
   await app.listen(3000);
